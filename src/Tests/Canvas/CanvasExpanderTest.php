12f611a6172496f77d5977e6c42fe9587f29dc0d
Malte Hübner <maltehuebner@gmx.org>
2016-11-15 00:46:26 +0100

    WIP at testcases.

--- src/Tests/Canvas/CanvasExpanderTest.php
+++ src/Tests/Canvas/CanvasExpanderTest.php
@@ -25,9 +25,9 @@ class CanvasExpanderTest extends \PHPUnit_Framework_TestCase
         $canvas = new Canvas();
 
         $expander = new CanvasExpander($canvas);
-        
+
         $coord = new Coord(53, 9);
-        
+
         $expander->expand($coord);
 
         $bounds = $expander->getBounds();
@@ -37,5 +37,56 @@ class CanvasExpanderTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($coord, $northWest);
         $this->assertEquals($coord, $southEast);
+    }
+
+    public function testCanvasExpander3()
+    {
+        $canvas = new Canvas();
+
+        $expander = new CanvasExpander($canvas);
+
+        $coord1 = new Coord(53, 9);
+        $coord2 = new Coord(52, 10);
+
+        $expander
+            ->expand($coord1)
+            ->expand($coord2);
+
+        $bounds = $expander->getBounds();
+
+        $northWest = $bounds->getNorthWest();
+        $southEast = $bounds->getSouthEast();
+
+        $this->assertEquals($coord1, $northWest);
+        $this->assertEquals($coord2, $southEast);
+    }
+
+    public function testCanvasExpander4()
+    {
+        $canvas = new Canvas();
+
+        $expander = new CanvasExpander($canvas);
+
+        $coord1 = new Coord(53, 9);
+        $coord2 = new Coord(52, 10);
+        $coord3 = new Coord(51, 11);
+        $coord4 = new Coord(50, 8);
+
+        $expander
+            ->expand($coord1)
+            ->expand($coord2)
+            ->expand($coord3)
+            ->expand($coord4);
+
+        $bounds = $expander->getBounds();
+
+        $actualNorthWest = $bounds->getNorthWest();
+        $actualSouthEast = $bounds->getSouthEast();
+
+        $expectedNorthWest = new Coord(53, 8);
+        $expectedSouthEast = new Coord(50, 11);
+
+        $this->assertEquals($expectedNorthWest, $actualNorthWest);
+        $this->assertEquals($expectedSouthEast, $actualSouthEast);
     }
 }
