469d6bdd59ed9e9ccc44d663d4f081c146527933
Malte Hübner <maltehuebner@gmx.org>
2016-11-15 00:34:52 +0100

    Trying a stupid CanvasExpanderTest.

---
+++ src/Tests/Canvas/CanvasExpanderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Caldera\MapPrinter\Tests\Canvas;
+
+use Caldera\GeoBasic\Coord\Coord;
+use Caldera\MapPrinter\Canvas\Canvas;
+use Caldera\MapPrinter\Canvas\CanvasExpander;
+use Caldera\MapPrinter\Tests\Mocks\SimpleMockedTrack;
+
+class CanvasExpanderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanvasExpander1()
+    {
+        $canvas = new Canvas();
+
+        $expander = new CanvasExpander($canvas);
+
+        $bounds = $expander->getBounds();
+
+        $this->assertNull($bounds);
+    }
+
+    public function testCanvasExpander2()
+    {
+        $canvas = new Canvas();
+
+        $expander = new CanvasExpander($canvas);
+        
+        $coord = new Coord(53, 9);
+        
+        $expander->expand($coord);
+    }
+}
