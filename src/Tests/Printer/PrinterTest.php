cc10473237bebb9d95dc6f875f51f2e1230a841c
Malte Hübner <maltehuebner@gmx.org>
2017-02-27 20:04:45 +0100

    Add printer test.

--- src/Tests/Printer/PrinterTest.php
+++ src/Tests/Printer/PrinterTest.php
@@ -3,21 +3,20 @@
 namespace Caldera\MapPrinter\Tests\Canvas;
 
 use Caldera\MapPrinter\Canvas\Canvas;
+use Caldera\MapPrinter\Printer\OsmMapPrinter;
 use Caldera\MapPrinter\Tests\Mocks\SimpleMockedTrack;
 use Caldera\MapPrinter\TileResolver\OsmTileResolver;
 
-class CanvasTest extends \PHPUnit_Framework_TestCase
+class PrinterTest extends \PHPUnit_Framework_TestCase
 {
     public function testCanvas1()
     {
-        $canvas = new Canvas();
+        $printer = new OsmMapPrinter();
+
         $mockedTrack = new SimpleMockedTrack();
 
-        $canvas
-            ->setZoomLevel(15)
+        $printer
             ->addTrack($mockedTrack)
-            ->calculateDimensions()
-            ->decorateTiles(new OsmTileResolver())
-            ->printElements();
+            ->execute();
     }
 }
